69a0707168eb022287bb50a32493876ad21c5478
micromix <40323669+Id-ware@users.noreply.github.com>
2023-06-14 09:02:23 +0300

    Update hidmouserptparser.cpp
    modified code to work with generic mouse, as USB Mouse Passthrough

--- hidmousereport/hidmouserptparser.cpp
+++ hidmousereport/hidmouserptparser.cpp
@@ -1,3 +1,10 @@
+// modified code to work with generic mouse, as USB Mouse Passthrough
+// modifed to work with generic USB 2.0 mouse: working mouse x and y positions, right click, left click, middle mouse click, 
+// also mouse scrollwheel all works (tested on generic usb mouse: "HP Wired mouse 1000")
+//
+// based of code from: https://github.com/mustaffxx/usb-host-shield-mouse
+// modifed by Id-ware: https://github.com/Id-ware/usb-host-shield-mouse
+
 #include "hidmouserptparser.h"
 
 
@@ -17,17 +24,14 @@ void HIDMouseReportParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8
 	}
 	previousButtonsState = buf[0];
 
-	int8_t xMovement = buf[2];
-	int8_t yMovement = buf[4];
-	int8_t scrollValue = buf[6];
-	int8_t tiltValue = buf[7];
+	int8_t xMovement = buf[1];
+	int8_t yMovement = buf[2];
+	int8_t scrollValue = buf[3];
 
 	if (xMovement > 127) xMovement -= 256;
 	if (yMovement > 127) yMovement -= 256;
 
 	if (xMovement != 0 || yMovement != 0 || scrollValue != 0)
 		onMouseMove(xMovement, yMovement, scrollValue);
-	
-	if (tiltValue != 0)
-		onTiltPress(tiltValue);
+
 }
