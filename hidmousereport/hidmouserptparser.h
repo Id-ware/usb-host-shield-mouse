b87f9f1010802bd8ac563f21da302ccc1bba44ba
micromix <40323669+Id-ware@users.noreply.github.com>
2023-06-14 09:01:49 +0300

    Update hidmouserptparser.h
    modified code to work with generic mouse, as USB Mouse Passthrough

--- hidmousereport/hidmouserptparser.h
+++ hidmousereport/hidmouserptparser.h
@@ -1,3 +1,10 @@
+// modified code to work with generic mouse, as USB Mouse Passthrough
+// modifed to work with generic USB 2.0 mouse: working mouse x and y positions, right click, left click, middle mouse click, 
+// also mouse scrollwheel all works (tested on generic usb mouse: "HP Wired mouse 1000")
+//
+// based of code from: https://github.com/mustaffxx/usb-host-shield-mouse
+// modifed by Id-ware: https://github.com/Id-ware/usb-host-shield-mouse
+
 #if !defined(__HIDMOUSERPTPARSER_H__)
 
 #define __HIDMOUSERPTPARSER_H__
@@ -7,12 +14,10 @@
 #define MOUSE_LEFT 		1
 #define MOUSE_RIGHT 	2
 #define MOUSE_MIDDLE 	4
-#define MOUSE_BUTTON4 	8
-#define MOUSE_BUTTON5 	16
+
 
 void onButtonUp(uint16_t buttonId) __attribute__((weak));
 void onButtonDown(uint16_t buttonId) __attribute__((weak));
-void onTiltPress(int8_t tiltValue) __attribute__((weak));
 void onMouseMove(int8_t xMovement, int8_t yMovement, int8_t scrollValue) __attribute__((weak));
 
 
